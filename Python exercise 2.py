78b44fbc06a70957936e71546bbc89a10662b61b
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-14 00:47:03 +1200

    Add files via upload

---
+++ Python exercise 2.py
@@ -0,0 +1,14 @@
+'''
+Input three integers and output following integers in a list.
+'''
+
+first = input("Input the first integer: ")
+second = input("Input the second integer: ")
+third = input("Input the third integer: ")
+
+mylist = []
+mylist.append(first)
+mylist.append(second)
+mylist.append(third)
+
+print(mylist)
