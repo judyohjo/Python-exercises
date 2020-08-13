1e5df01eb3c93337e053963e21ad1f0299814914
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-14 00:59:33 +1200

    Add files via upload

---
+++ Python exercise 3.py
@@ -0,0 +1,9 @@
+'''
+Input a string and input a number and print out the number of times of the string in a new line.
+'''
+
+mystring = input("Input a string: ")
+num = int(input("Input a number: "))
+for i in range(0, num):
+    print(mystring)
+    
