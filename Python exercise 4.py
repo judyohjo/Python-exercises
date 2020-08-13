f07c80f355d582a2192bf5bdc568fa3cf994212c
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-14 01:03:39 +1200

    Add files via upload

---
+++ Python exercise 4.py
@@ -0,0 +1,8 @@
+'''
+Output words with length 5 or more (5 inclusive) from list1.
+'''
+
+list1 = ['apple', 'encyclopedia', 'library', 'then', 'red', 'plant']
+for i in list1:
+    if len(i) >= 5:
+        print(i)
