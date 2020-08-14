5dac2561e028a7f7be9aef78aa7ec5f25049b5aa
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-15 01:08:04 +1200

    Add files via upload

---
+++ Python exercise 5.py
@@ -0,0 +1,9 @@
+'''
+Output words with length 5 or more (5 inclusive) from the sentence.
+'''
+
+mystring = 'Python is fun and awesome'
+new_string = mystring.split()
+for i in new_string:
+    if len(i) >= 5:
+        print(i)
