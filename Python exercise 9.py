fe6a46e0c879e149fac830a930d3c7661f0fe542
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-16 01:29:19 +1200

    Add files via upload

---
+++ Python exercise 9.py
@@ -0,0 +1,12 @@
+'''
+Calculate the sum and average of list of numbers.
+'''
+
+list1 = ['6', '32', '15', '22', '5']
+total = 0
+for i in list1:
+    total += int(i)
+
+print("Sum:", total)
+print("Average:", (total/len(list1)))
+
