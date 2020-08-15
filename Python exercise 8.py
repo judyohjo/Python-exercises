a22b4424914bd093cb8f4ba6f5ca4559a4d38262
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-16 01:28:51 +1200

    Add files via upload

---
+++ Python exercise 8.py
@@ -0,0 +1,13 @@
+'''
+Check if the string is in word/alphabet and if it is print the word/alphabet.
+'''
+
+mystring = 'Python is 4 fun and awesome. I want to learn 5 languages.'
+mylist = mystring.split()
+for i in mylist:
+    if i.isdigit():
+        pass
+    else:
+        print(i)
+    
+
