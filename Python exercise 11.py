bbcd285b623f9c9b06636143fc6a68a61f7b7852
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-17 01:23:13 +1200

    Add files via upload

---
+++ Python exercise 11.py
@@ -0,0 +1,13 @@
+'''
+Input a number and print the number of times of "X"
+(eg. if number is 3, print...
+X
+XX
+XXX)
+'''
+
+num = int(input("Input a number: "))
+for i in range(1, num+1):
+    print(i*"X")
+
+
