ad3ba9c92bfc6ffcd5f9c8ad8f759ac5141242f8
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-18 00:30:01 +1200

    Add files via upload

---
+++ Python exercise 14.py
@@ -0,0 +1,12 @@
+'''
+Input height and width and calculate perimeter and area of a square.
+'''
+
+height = int(input("Height: "))
+width = int(input("Width: "))
+perimeter = (2 * height) + (2 * width)
+area = height * width
+print("Perimeter:", perimeter)
+print("Area:", area)
+
+
