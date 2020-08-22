cb04d0099cec9d1db7018d4c3a35f5f2bb35dcec
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-23 00:38:58 +1200

    Add files via upload

---
+++ Python exercise 17.py
@@ -0,0 +1,17 @@
+'''
+Subtract num2 from num1.
+'''
+def subtract(num1, num2):
+    total = num1 - num2
+    return total
+
+
+print("3 - 4 =", subtract(3, 4))
+print("22 - 2 =", subtract(22, 2))
+print("-6 - 40 =", subtract(-6, 40))
+print("-5 - (-4) =", subtract(-5, (-4)))
+print("0 - 0 =", subtract(0, 0))
+
+    
+    
+
