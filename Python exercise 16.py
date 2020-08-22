1a25ca68ffad370abcf5a4a9b7917afe8e0804e8
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-23 00:35:25 +1200

    Add files via upload

---
+++ Python exercise 16.py
@@ -0,0 +1,17 @@
+'''
+Add two numbers.
+'''
+def add(num1, num2):
+    total = num1 + num2
+    return total
+
+
+print("3 + 4 =", add(3, 4))
+print("15 + 20 =", add(15, 20))
+print("-6 + 99 =", add(-6, 99))
+print("204 + 140 =", add(204, 140))
+print("13 + 0 =", add(13, 0))
+
+    
+    
+
