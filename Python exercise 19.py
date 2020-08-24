ec001d7b7fb03dc5603a19ecff67aa4e2ea6b9e3
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-25 01:13:01 +1200

    Add files via upload

---
+++ Python exercise 19.py
@@ -0,0 +1,17 @@
+'''
+Divide num2 from num1 (2dp).
+'''
+def divide(num1, num2):
+    total = float(num1 / num2)
+    return total
+
+
+print("3 / 4 = {:.2f}".format(divide(3, 4)))
+print("22 / 2 = {:.2f}".format(divide(22, 2)))
+print("-6 / 40 = {:.2f}".format(divide(-6, 40)))
+print("-15 / (-3) = {:.2f}".format(divide(-15, (-3))))
+
+
+    
+    
+
