44c76d57715157c63c3beabb9bf2aa00b2a6aea2
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-25 01:06:47 +1200

    Add files via upload

---
+++ Python exercise 18.py
@@ -0,0 +1,17 @@
+'''
+Multiply num1 and num2.
+'''
+def multiply(num1, num2):
+    total = num1 * num2
+    return total
+
+
+print("3 * 4 =", multiply(3, 4))
+print("22 * 2 =", multiply(22, 2))
+print("-6 * 40 =", multiply(-6, 40))
+print("-5 * (-4) =", multiply(-5, (-4)))
+print("0 * 0 =", multiply(0, 0))
+
+    
+    
+
