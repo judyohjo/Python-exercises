bc89a96da866f835cba0938f8f398e7e151febcc
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-26 00:47:54 +1200

    Update Python exercise 19.py

--- Python exercise 19.py
+++ Python exercise 19.py
@@ -5,8 +5,8 @@ def divide(num1, num2):
     total = float(num1 / num2)
     return total
 
-
-print("3 / 4 = {:.2f}".format(divide(3, 4)))
+//test cases 
+print("3 / 4 = {:.2f}".format(divide(3, 4))) 
 print("22 / 2 = {:.2f}".format(divide(22, 2)))
 print("-6 / 40 = {:.2f}".format(divide(-6, 40)))
 print("-15 / (-3) = {:.2f}".format(divide(-15, (-3))))
