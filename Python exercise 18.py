0ad1742c16a3483dd602a282fc653a608f19f8ed
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-08-26 00:48:15 +1200

    Update Python exercise 18.py

--- Python exercise 18.py
+++ Python exercise 18.py
@@ -5,7 +5,7 @@ def multiply(num1, num2):
     total = num1 * num2
     return total
 
-
+//test cases
 print("3 * 4 =", multiply(3, 4))
 print("22 * 2 =", multiply(22, 2))
 print("-6 * 40 =", multiply(-6, 40))
