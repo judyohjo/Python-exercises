330863659e0b7c68d3f627c50524622e1c86a3d3
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-09-12 08:57:29 +0900

    Add files via upload

---
+++ Python exercise 25.py
@@ -0,0 +1,16 @@
+'''
+Randomly pick a word from a sentence and see if it's a vowel.
+'''
+
+import random
+
+string = input("Input a phrase/sentence: ")
+vowel = 'a, e, i, o, u'
+random_num = random.randrange(0, len(string)-1)
+random_alphabet = string[random_num]
+
+if random_alphabet in vowel:
+    print("This alphabet, " + random_alphabet + ", is a vowel.")
+else:
+    print("This alphabet, " + random_alphabet + ", is not a vowel.")
+
