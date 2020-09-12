eabc5e2808c652e7e0f5290cefb32d753ba8a75e
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-09-12 23:30:33 +0900

    Add files via upload

---
+++ Python exercise 26.py
@@ -0,0 +1,16 @@
+'''
+Randomly pick a name from a list and form a pair.
+'''
+
+import random
+
+random_name = []
+roster = ['Ben', 'Alice', 'Henry', 'Sam', 'Jane', 'Kim', 'Dan', 'Clara', 'Tim', 'Michelle', 'Jim']
+for i in range(0, 2):
+    random_num = random.randrange(0, len(roster)-1)
+    random_name.append(roster[random_num])
+if random_name[0] != random_name[1]:
+        print(random_name[0], "and", random_name[1], "are pairs.")
+
+
+
