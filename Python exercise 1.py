77e5eee3347749accf7f8adbf58091192008469c
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-01 13:41:28 +1300

    Update Python exercise 1.py

--- Python exercise 1.py
+++ Python exercise 1.py
@@ -1,5 +1,5 @@
 # Exercise 1 - Output each word from list1 in a new line
 
-    list1 = ['blue', 'apple', 'tree', 'green', 'animal']
-    for i in list1:
-        print(i)
+list1 = ['blue', 'apple', 'tree', 'green', 'animal']
+for i in list1:
+    print(i)
