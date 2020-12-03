6413920aa081a6b7b03112ffc3cbab5a6e800550
judyohjo <68041382+judyohjo@users.noreply.github.com>
2020-12-03 14:53:32 +1300

    Update Python exercise 4.py

--- Python exercise 4.py
+++ Python exercise 4.py
@@ -1,5 +1,5 @@
 '''
-Output words with length 5 or more (5 inclusive) from list1.
+Exercise 4 - Output words with length 5 or more (5 inclusive) from list1.
 '''
 
 list1 = ['apple', 'encyclopedia', 'library', 'then', 'red', 'plant']
